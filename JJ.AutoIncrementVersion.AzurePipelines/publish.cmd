efde0f9bb793bff258ef8a21b18c6565ff0d0ddf
jjvanzon <jjvanzon79@gmail.com>
2026-03-19 03:15:44 +0100

    🧹 JJ.AutoIncrementVersion.AzurePipelines - Clear out junk from publish.cmd script.

--- JJ.AutoIncrementVersion.AzurePipelines/publish.cmd
+++ JJ.AutoIncrementVersion.AzurePipelines/publish.cmd
@@ -1,20 +1,8 @@
-rem [ PRELIMINARY SCRIPT ]
+@echo off
 
 rem install node js
 
 winget install OpenJS.NodeJS.LTS
-
-rem -----
-
-rem Install npm = node package manager
-rem lts = long term support
-rem nvm = node version manager
-rem Turs out unnecessary.
-
-rem nvm install lts
-rem nvm use lts 
-
-rem -----
 
 rem Installs command line tooling for Azure DevOps, formerly VSTS, formerly TFS
 rem Hopefully just skips if already installed
@@ -24,6 +12,6 @@ npm install -g tfx-cli
 
 rem Create package
 rem --rev-version : increments version automatically
-rem Iutputs a .visx file
+rem Outputs a .visx file
 
 tfx extension create --manifest-globs vss-extension.json --rev-version
