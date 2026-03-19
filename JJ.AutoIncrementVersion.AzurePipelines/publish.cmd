c3607c29d331b7523afd30f7241a3ec7ba284d6b
jjvanzon <jjvanzon79@gmail.com>
2026-03-19 03:10:32 +0100

    📦 JJ.AutoIncrementVersion.AzurePipelines - Commands in publish.cmd run manually and produced a vsix.

--- JJ.AutoIncrementVersion.AzurePipelines/publish.cmd
+++ JJ.AutoIncrementVersion.AzurePipelines/publish.cmd
@@ -4,15 +4,21 @@ rem install node js
 
 winget install OpenJS.NodeJS.LTS
 
+rem -----
+
 rem Install npm = node package manager
 rem lts = long term support
 rem nvm = node version manager
+rem Turs out unnecessary.
 
-nvm install lts
-nvm use lts 
+rem nvm install lts
+rem nvm use lts 
+
+rem -----
 
 rem Installs command line tooling for Azure DevOps, formerly VSTS, formerly TFS
 rem Hopefully just skips if already installed
+rem -g = global
 
 npm install -g tfx-cli
 
