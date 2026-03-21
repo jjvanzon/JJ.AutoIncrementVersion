8cb35ef7189a1e3d25129b0ed8c201167825b84f
jjvanzon <jjvanzon79@gmail.com>
2026-03-21 01:12:12 +0100

    📜 AutoIncrementVersion.AzurePipelines - pack.cmd AI draft - Read BuildNum.xml; Get version from json and parse; Build new version with build num; Write to overrides.json; Pass overrides.json to packaging command.

--- JJ.AutoIncrementVersion.AzurePipelines/pack.cmd
+++ JJ.AutoIncrementVersion.AzurePipelines/pack.cmd
@@ -14,9 +14,25 @@ rem -g = global
 call npm install -g tfx-cli
 
 echo(
+echo Read BuildNum.xml
+echo -----------------
+echo(
+for /f %%i in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "([xml](Get-Content '..\BuildNum.xml' -Raw)).Project.PropertyGroup.BuildNum"') do set BUILDNUM=%%i
+for /f %%i in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "(Get-Content 'vss-extension.json' -Raw | ConvertFrom-Json).version"') do set CURRENT_VERSION=%%i
+for /f "tokens=1,2 delims=." %%a in ("%CURRENT_VERSION%") do (
+  set MAJOR=%%a
+  set MINOR=%%b
+)
+set EXT_VERSION=%MAJOR%.%MINOR%.%BUILDNUM%
+
+echo Using extension version: %EXT_VERSION%
+
+echo { "version": "%EXT_VERSION%" } > _tfx-overrides.json
+
+echo(
 echo Create VSIX Package
 echo -------------------
 echo(
-rem --rev-version : increments version automatically
-call tfx extension create --manifest-globs vss-extension.json --rev-version
-rem call tfx extension create --manifest-globs vss-extension.json
+call tfx.cmd extension create --manifest-globs vss-extension.json --overrides-file _tfx-overrides.json
+
+del /q _tfx-overrides.json
